fede7c49148d5c83012a3e4a97f1cbefa89375ce
Matt Styles <matt@veryfizzyjelly.com>
2013-05-14 23:17:37 +0100

    Creates dummy data for database if none exists

--- server/db/index.js
+++ server/db/index.js
@@ -11,7 +11,7 @@ var server  = new mongo.Server( appConfig.database.host, appConfig.database.port
 // Open a client to mongo
 client.open( function( err, client ) {
     if ( err ) {
-        console.log( 'Database connection error' );
+        console.log( 'Database connection error\nis mongod running?' );
         console.log( err );
         return;
     }
@@ -20,17 +20,56 @@ client.open( function( err, client ) {
 
     // Always create a new collection
     var db = client.db( appConfig.database.name );
-    db.createCollection( appConfig.database.collection, function( err, col ) {
+    db.createCollection( appConfig.database.collection, function( err, collection ) {
         if ( err ) {
             console.log( 'Error creating collection' );
             console.log( err );
-            return;
+            return err;
         }
 
-        console.log( 'Collection created' );
+        // If collection is empty then create some dummy data
+        collection.find({}).toArray( function( err, items ) {
+            if ( err ) {
+                console.log( 'Error accessing collection' );
+                console.log( err );
+                return err;
+            }
+
+            if ( !items.length > 0 ) {
+                createDummyCollection( collection );
+            }
+        } );
     } );
 
 } );
 
 
+// Create dummy data for the collection
+var createDummyCollection = function( collection ) {
+    console.log( 'Creating collection...' );
+
+    var dummy = [{
+        name: 'Albert',
+        address: '21 Jump St',
+        phone: '123 456 789'
+    }, {
+        name: 'Bert',
+        address: '15 Sesame St',
+        phone: '987 654 321'
+    }, {
+        name: 'Cecil',
+        address: '7 Rue de la Vio',
+        phone: '0101 98 98'
+    } ];
+
+    collection.insert( dummy, function( err, res ) {
+        if ( err ) {
+            console.log( 'Error inserting collection data' );
+            console.log( err );
+            return err;
+        }
+
+        console.log( 'Collection populated' );
+    } );
+};
 
