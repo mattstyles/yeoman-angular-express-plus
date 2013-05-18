a931cbc4c4194f85000c4ba69692ec4f62c0a60e
Matt Styles <matt@veryfizzyjelly.com>
2013-05-18 08:16:45 +0100

    Changes to connect
    Sets up the connect server to work if the distribution folder name changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,7 +74,7 @@ module.exports = function (grunt) {
             options: {
                 port: 9000,
                 // Change this to '0.0.0.0' to access the server from outside.
-                hostname: 'localhost'
+                hostname: '0.0.0.0'
             },
             livereload: {
                 options: {
@@ -82,7 +82,7 @@ module.exports = function (grunt) {
                         return [
                             lrSnippet,
                             mountFolder( connect, '.tmp' ),
-                            mountFolder( connect, yeomanConfig.app )
+                            mountFolder( connect, yeoman.app )
                         ];
                     }
                 }
@@ -94,7 +94,7 @@ module.exports = function (grunt) {
                         return [
                             lrSnippet,
                             mountFolder( connect, '.tmp' ),
-                            mountFolder( connect, require('path').join( yeomanConfig.dist, yeomanConfig.app ) )
+                            mountFolder( connect, require('path').join( yeoman.dist, yeoman.app ) )
                         ];
                     }
                 }
